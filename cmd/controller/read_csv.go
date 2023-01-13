059d5edb7b499e32c71baba7343253ce1839b6d0
adrianosantana <ans3@cin.ufpe.br>
2023-01-13 15:03:43 -0300

    test: Should return a list of movies read from CSV

--- cmd/controller/read_csv.go
+++ cmd/controller/read_csv.go
@@ -1,16 +1,46 @@
 package controller
 
 import (
+	"encoding/csv"
 	"errors"
 	"os"
+	"strconv"
+
+	"github.com/AdrianoSantana/go-read-csv/cmd/models"
 )
 
 var fileNotFound error = errors.New("Não foi possivel encontrar um arquivo com o caminho especificado")
 
-func ReadCSV(filePath string) (string, error) {
+func ReadCSV(filePath string) ([]models.Movie, error) {
+	movies := []models.Movie{}
+
 	_, err := os.Stat(filePath)
 	if err != nil {
-		return "", fileNotFound
+		return nil, fileNotFound
 	}
-	return "", nil
+
+	csvFile, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer csvFile.Close()
+
+	lines, err := csv.NewReader(csvFile).ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, line := range lines {
+		id, err := strconv.Atoi(line[0])
+		if err != nil {
+			continue
+		}
+		mv := models.Movie{
+			ID:     id,
+			Title:  line[1],
+			Genres: line[2],
+		}
+		movies = append(movies, mv)
+	}
+	return movies, nil
 }
