6a955486cf07ac18989b186b113285fa509d94d3
adrianosantana <ans3@cin.ufpe.br>
2023-01-31 15:29:05 -0300

    feat: adicionado goroutine

--- cmd/repositories/sqlite_repository.go
+++ cmd/repositories/sqlite_repository.go
@@ -3,11 +3,15 @@ package repositories
 import (
 	"database/sql"
 	"log"
+	"sync"
 
 	"github.com/AdrianoSantana/go-read-csv/cmd/models"
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+var wg sync.WaitGroup
+var mut sync.Mutex
 
 type MovieRepositorySqlite struct{}
 
@@ -24,22 +28,33 @@ func (mv MovieRepositorySqlite) Insert(movies []models.Movie) (insertedMovies in
 	}
 
 	for _, movie := range movies {
-		smt, err := db.Prepare("insert into movies(title, genres) VALUES(?, ?)")
-		if err != nil {
-			log.Fatal(err)
-			continue
-		}
-		_, err = smt.Exec(movie.Title, movie.Genres)
-		if err != nil {
-			log.Fatal(err)
-			continue
-		}
-		insertedMovies += 1
+		go saveMovieInDB(db, movie)
+		wg.Add(1)
 	}
+	wg.Wait()
 	return
 }
 
-func createOrGetTable(db *sql.DB) (error) {
+func saveMovieInDB(db *sql.DB, movie models.Movie) {
+	defer wg.Done()
+	mut.Lock()
+	defer mut.Unlock()
+	insertInDatabase(db, movie)
+
+}
+
+func insertInDatabase(db *sql.DB, movie models.Movie) {
+	smt, err := db.Prepare("insert into movies(title, genres) VALUES(?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = smt.Exec(movie.Title, movie.Genres)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createOrGetTable(db *sql.DB) error {
 	_, err := db.Query("CREATE TABLE IF NOT EXISTS movies (id integer PRIMARY KEY, title TEXT NOT NULL, genres TEXT NOT NULL)")
 	if err != nil {
 		return err
@@ -47,6 +62,6 @@ func createOrGetTable(db *sql.DB) (error) {
 	return nil
 }
 
-func isConnected(db *sql.DB) (error) {
+func isConnected(db *sql.DB) error {
 	return db.Ping()
 }
